bba9e0880059d51afe4112884470264d7489aca5
GalchynskaSofiia <113577844+GalchynskaSofiia@users.noreply.github.com>
2022-09-20 14:52:41 +0300

    Update ЛБ4(2).py

--- ЛБ4(2).py
+++ ЛБ4(2).py
@@ -1,7 +1,9 @@
-from cmath import sin
 import math
+def func1(x1, x2, x3):
+    m = (x1 ** 3 + x2) * (x3 - math.log(abs(x3)) - math.sin(x2)) + ((x1 **3 + x3) ** (1/3))
+    return(m)
 a = float(input("Введіть значення a: "))
 x = float(input("Введіть значення x: "))
 y = float(input("Введіть значення y: "))
-m = (x ** 3 + a) * (y - math.log(abs(y)) - sin(a)) + ((x **3 + y) ** (1/3))
+m = func1(x, a, y)
 print("Z =", m)
